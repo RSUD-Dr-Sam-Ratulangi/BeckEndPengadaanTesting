20e60ce7a95155b3ff4051924bb7894fd4f57304
Devaliel <darkzill0410@gmail.com>
2023-04-14 21:54:07 +0800

    rebuild database model

---
+++ src/main/java/com/example/pengadaanrsudsamrat/vendor/VendorRepository.java
@@ -0,0 +1,9 @@
+package com.example.pengadaanrsudsamrat.vendor;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository extends JpaRepository <VendorModel,Long> {
+}
