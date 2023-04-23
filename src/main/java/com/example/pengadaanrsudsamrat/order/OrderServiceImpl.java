f925b76147f703611996af5da85737a6b6479f35
Devaliel <darkzill0410@gmail.com>
2023-04-23 16:17:08 +0800

    masih bug soal create order > mar klo add product to order > jadi cman setiak kali add product to order berdasarkan no id product data yang di dalam dia hpus dlu baru dia update mar klo click update trus dia tetap batambah nd ta hapu

--- src/main/java/com/example/pengadaanrsudsamrat/order/OrderServiceImpl.java
+++ src/main/java/com/example/pengadaanrsudsamrat/order/OrderServiceImpl.java
@@ -1,86 +1,252 @@
 package com.example.pengadaanrsudsamrat.order;
 
-import com.example.pengadaanrsudsamrat.DTO.OrderDTO;
-import com.example.pengadaanrsudsamrat.DTO.OrderItemDTO;
+
+
+import com.example.pengadaanrsudsamrat.DTO.*;
+
+import com.example.pengadaanrsudsamrat.exception.NotEnoughStockException;
+import com.example.pengadaanrsudsamrat.orderitem.OrderItemModel;
+import com.example.pengadaanrsudsamrat.orderitem.OrderItemRepository;
 import com.example.pengadaanrsudsamrat.payment.PaymentModel;
 import com.example.pengadaanrsudsamrat.payment.PaymentRepository;
+
 import com.example.pengadaanrsudsamrat.products.ProductModel;
 import com.example.pengadaanrsudsamrat.products.ProductRepository;
-import com.example.pengadaanrsudsamrat.vendor.VendorRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
-    private final VendorRepository vendorRepository;
-    private final ProductRepository productRepository;
+
+    private final OrderItemRepository orderItemRepository;
     private final PaymentRepository paymentRepository;
     private final ModelMapper modelMapper;
+    private final ProductRepository productRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository, VendorRepository vendorRepository,
-                            ProductRepository productRepository, PaymentRepository paymentRepository,
-                            ModelMapper modelMapper) {
+    public OrderServiceImpl(OrderRepository orderRepository, OrderItemRepository orderItemRepository, PaymentRepository paymentRepository,
+                            ModelMapper modelMapper, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
-        this.vendorRepository = vendorRepository;
-        this.productRepository = productRepository;
+        this.orderItemRepository = orderItemRepository;
+
         this.paymentRepository = paymentRepository;
         this.modelMapper = modelMapper;
+        this.productRepository = productRepository;
     }
 
     @Override
-    public OrderDTO createOrder(OrderDTO orderDTO) {
-        OrderModel orderModel = modelMapper.map(orderDTO, OrderModel.class);
+    public OrderResponseDTO createOrder(OrderRequestDTO orderRequestDTO) {
+        OrderModel orderModel = new OrderModel();
+        orderModel.setOrderDate(new Date());
+        List<OrderItemModel> orderItemModels = new ArrayList<>();
+
+        for (Long orderItemId : orderRequestDTO.getOrderItemIds()) {
+            OrderItemModel orderItemModel = orderItemRepository.findById(orderItemId)
+                    .orElseThrow(EntityNotFoundException::new);
+            orderItemModels.add(orderItemModel);
+        }
+
+        orderModel.setOrderItems(orderItemModels);
+
+        OrderModel savedOrderModel = orderRepository.save(orderModel);
+
         double totalAmount = 0.0;
+        for (OrderItemModel orderItemModel : orderItemModels) {
+            totalAmount += orderItemModel.getProduct().getPrice() * orderItemModel.getQuantity();
+        }
 
-        if (orderDTO.getOrderItems() != null) {
-            List<OrderItemModel> orderItemModels = new ArrayList<>();
-            for (OrderItemDTO orderItemDTO : orderDTO.getOrderItems()) {
-                OrderItemModel orderItemModel = modelMapper.map(orderItemDTO, OrderItemModel.class);
-                ProductModel productModel = productRepository.findById(orderItemDTO.getProduct().getId()).orElseThrow(EntityNotFoundException::new);
+        PaymentModel paymentModel = new PaymentModel();
+        paymentModel.setAmount(BigDecimal.valueOf(totalAmount));
 
-                // Subtract the quantity of the ordered product from the available quantity
-                int newQuantity = productModel.getQuantity() - orderItemDTO.getQuantity();
-                productModel.setQuantity(newQuantity);
+        PaymentModel savedPaymentModel = paymentRepository.save(paymentModel);
 
-                orderItemModel.setProduct(productModel);
-                orderItemModel.setOrder(orderModel);
-                orderItemModels.add(orderItemModel);
+        savedOrderModel.setPayment(savedPaymentModel);
+        orderRepository.save(savedOrderModel);
 
-                // Add the price of the ordered product to the total amount
-                totalAmount += orderItemDTO.getQuantity() * productModel.getPrice();
+        return modelMapper.map(savedOrderModel, OrderResponseDTO.class);
+    }
+
+    /*
+    @Override
+    public OrderItemResponseDTO createOrderItem(OrderItemRequestDTO orderItemRequestDTO) {
+        // Check if product exists
+        ProductModel productModel = productRepository.findById(orderItemRequestDTO.getProductId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        // Check if there is enough quantity
+        if (productModel.getQuantity() < orderItemRequestDTO.getQuantity()) {
+            throw new IllegalArgumentException("Not enough quantity available");
+        }
+
+        // Get or create the order
+        OrderModel orderModel = orderRepository.findById(orderItemRequestDTO.getOrderItemid())
+                .orElseGet(() -> {
+                    OrderModel newOrderModel = new OrderModel();
+                    newOrderModel.setOrderDate(new Date());
+                    return orderRepository.save(newOrderModel);
+                });
+
+        // Create order item and set product, quantity and order
+        OrderItemModel orderItemModel = new OrderItemModel();
+        orderItemModel.setProduct(productModel);
+        orderItemModel.setQuantity(orderItemRequestDTO.getQuantity());
+        orderItemModel.setOrder(orderModel);
+
+        // Decrease product quantity
+        productModel.setQuantity(productModel.getQuantity() - orderItemRequestDTO.getQuantity());
+        productRepository.save(productModel);
+
+        // Save order item and return response
+        OrderItemModel savedOrderItemModel = orderItemRepository.save(orderItemModel);
+        return modelMapper.map(savedOrderItemModel, OrderItemResponseDTO.class);
+    }
+    */
+
+    @Override
+    public OrderResponseDTO updateOrderItemList(Long orderId, List<OrderItemRequestDTO> orderItemRequestDTOList) {
+        // Find the order by ID
+        OrderModel orderModel = orderRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+
+        // Get the existing order items
+        List<OrderItemModel> existingOrderItems = orderModel.getOrderItems();
+
+        // Create a new list to store the updated order items
+        List<OrderItemModel> updatedOrderItems = new ArrayList<>(existingOrderItems);
+
+        // Loop through each order item in the request
+        for (OrderItemRequestDTO orderItemRequestDTO : orderItemRequestDTOList) {
+            // Check if there is an existing order item with the same product ID
+            boolean found = false;
+            for (OrderItemModel existingOrderItem : existingOrderItems) {
+                if (existingOrderItem.getProduct().getId().equals(orderItemRequestDTO.getProductId())) {
+                    // Update the quantity of the existing order item
+                    existingOrderItem.setQuantity(existingOrderItem.getQuantity() + orderItemRequestDTO.getQuantity());
+                    found = true;
+                    break;
+                }
             }
-            orderModel.setOrderItems(orderItemModels);
+            if (!found) {
+                // Create a new order item and add it to the updated list
+                OrderItemModel newOrderItem = new OrderItemModel();
+                ProductModel product = productRepository.findById(orderItemRequestDTO.getProductId())
+                        .orElseThrow(() -> new EntityNotFoundException("Product not found"));
+                newOrderItem.setProduct(product);
+                newOrderItem.setQuantity(orderItemRequestDTO.getQuantity());
+                updatedOrderItems.add(newOrderItem);
+            }
         }
 
-        if (orderDTO.getPayment() != null) {
-            PaymentModel paymentModel = modelMapper.map(orderDTO.getPayment(), PaymentModel.class);
-            paymentModel.setAmount(BigDecimal.valueOf(totalAmount));
-            paymentModel.setOrder(orderModel);
-            orderModel.setPayment(paymentModel);
+        // Update the order with the updated order items
+        orderModel.setOrderItems(updatedOrderItems);
+        OrderModel savedOrderModel = orderRepository.save(orderModel);
+
+        // Recalculate the total amount due for the order
+        double totalAmount = 0.0;
+        for (OrderItemModel orderItemModel : savedOrderModel.getOrderItems()) {
+            totalAmount += orderItemModel.getProduct().getPrice() * orderItemModel.getQuantity();
         }
 
-        OrderModel savedOrder = orderRepository.save(orderModel);
+        // Update the payment amount and save it
+        PaymentModel paymentModel = savedOrderModel.getPayment();
+        paymentModel.setAmount(BigDecimal.valueOf(totalAmount));
+        PaymentModel savedPaymentModel = paymentRepository.save(paymentModel);
 
-        return modelMapper.map(savedOrder, OrderDTO.class);
+        // Update the order with the payment and save it again
+        savedOrderModel.setPayment(savedPaymentModel);
+        OrderModel updatedOrderModel = orderRepository.save(savedOrderModel);
+
+        return modelMapper.map(updatedOrderModel, OrderResponseDTO.class);
     }
 
 
 
-    public List<OrderDTO> findAll() {
-        List<OrderModel> orders = orderRepository.findAll();
-        return orders.stream()
-                .map(order -> modelMapper.map(order, OrderDTO.class))
-                .collect(Collectors.toList());
+    @Override
+    public OrderResponseDTO addOrderItemsToOrder(Long orderId, List<OrderItemRequestDTO> orderItems) {
+        OrderModel orderModel = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        List<OrderItemModel> existingOrderItems = orderModel.getOrderItems();
+
+        for (OrderItemRequestDTO orderItemDTO : orderItems) {
+            ProductModel productModel = productRepository.findById(orderItemDTO.getProductId())
+                    .orElseThrow(EntityNotFoundException::new);
+
+            int quantityToAdd = orderItemDTO.getQuantity();
+
+            // Check if the product has enough stock
+            if (productModel.getQuantity() < quantityToAdd) {
+                throw new NotEnoughStockException(productModel.getName());
+            }
+
+            // Update the product quantity and save it
+            productModel.setQuantity(productModel.getQuantity() - quantityToAdd);
+            productRepository.save(productModel);
+
+            // Check if the order already contains the product
+            boolean orderContainsProduct = false;
+            for (OrderItemModel existingOrderItem : existingOrderItems) {
+                if (existingOrderItem.getProduct().equals(productModel)) {
+                    existingOrderItem.setQuantity(existingOrderItem.getQuantity() + quantityToAdd);
+                    orderContainsProduct = true;
+                    break;
+                }
+            }
+
+            // If the order doesn't contain the product, create a new OrderItem for it
+            if (!orderContainsProduct) {
+                OrderItemModel orderItemModel = new OrderItemModel();
+                orderItemModel.setProduct(productModel);
+                orderItemModel.setQuantity(quantityToAdd);
+                orderItemModel.setOrder(orderModel);
+                existingOrderItems.add(orderItemModel);
+            }
+        }
+
+        orderModel.setOrderItems(existingOrderItems);
+        OrderModel savedOrderModel = orderRepository.save(orderModel);
+
+        double totalAmount = 0.0;
+        for (OrderItemModel orderItemModel : existingOrderItems) {
+            totalAmount += orderItemModel.getProduct().getPrice() * orderItemModel.getQuantity();
+        }
+
+        PaymentModel paymentModel = savedOrderModel.getPayment();
+        if (paymentModel == null) {
+            paymentModel = new PaymentModel();
+        }
+        paymentModel.setAmount(BigDecimal.valueOf(totalAmount));
+        PaymentModel savedPaymentModel = paymentRepository.save(paymentModel);
+
+        savedOrderModel.setPayment(savedPaymentModel);
+        orderRepository.save(savedOrderModel);
+
+        return modelMapper.map(savedOrderModel, OrderResponseDTO.class);
     }
+
+
+
+
+
+    @Override
+    public OrderResponseDTO getOrderById(Long orderId) {
+        OrderModel orderModel = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        return modelMapper.map(orderModel, OrderResponseDTO.class);
+    }
+
 
 
 }
+
+
+
+
 
